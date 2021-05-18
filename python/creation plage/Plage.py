ce5b9de6cd2d4846896a3b8bdfd61e525f07d9c9
Théo <tgueuret@live.fr>
2021-05-18 17:56:06 +0200

    add functions in class Plage

--- python/creation plage/Plage.py
+++ python/creation plage/Plage.py
@@ -1,7 +1,16 @@
 class Plage:
 
-    
+    def __init__(self, num = None):
+        if num is None:
+            self.num = 0
+        else:
+            self.num = num
 
+    def getNum(self):
+        return self.num
+    
+    def setNum(self, num):
+        self.num = num
 
     def print_hello(self):
         print("Hello World !")
--- python/creation plage/main.py
+++ python/creation plage/main.py
@@ -1,3 +1,15 @@
 #Théo :)
 
 from Plage import Plage
+
+my_plage = Plage()
+my_plage.print_hello()
+num = my_plage.getNum()
+print(num)
+my_plage.setNum(100)
+num = my_plage.getNum()
+print(num)
+my_plage.print_hello()
+my_palge2 = Plage(7)
+num = my_palge2.getNum()
+print(num)
