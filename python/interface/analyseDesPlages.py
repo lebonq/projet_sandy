44c6a434993c3dc473d6d4542a6634ec9fe1fe6f
Uncxnditixnally <charlottepook@hotmail.fr>
2021-05-25 14:27:52 +0200

    v1 analyse des Plages

--- python/interface/analyseDesPlages.py
+++ python/interface/analyseDesPlages.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'analyseDesPlages.ui'
+# Form implementation generated from reading ui file 'test.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.12
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1115, 889)
+        MainWindow.resize(1126, 946)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.descriptionPlastique = QtWidgets.QLabel(self.centralwidget)
+        self.descriptionPlastique.setFrameShape(QtWidgets.QFrame.Box)
+        self.descriptionPlastique.setObjectName("descriptionPlastique")
+        self.gridLayout_2.addWidget(self.descriptionPlastique, 8, 0, 2, 3)
         self.Titre = QtWidgets.QLabel(self.centralwidget)
         self.Titre.setEnabled(True)
-        self.Titre.setGeometry(QtCore.QRect(0, 0, 1121, 121))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -34,31 +40,39 @@ class Ui_MainWindow(object):
         self.Titre.setScaledContents(False)
         self.Titre.setAlignment(QtCore.Qt.AlignCenter)
         self.Titre.setObjectName("Titre")
+        self.gridLayout_2.addWidget(self.Titre, 0, 0, 1, 3)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.nomPlage = QtWidgets.QLabel(self.centralwidget)
-        self.nomPlage.setGeometry(QtCore.QRect(0, 130, 131, 31))
+        self.nomPlage.setEnabled(True)
         self.nomPlage.setFrameShape(QtWidgets.QFrame.Box)
         self.nomPlage.setObjectName("nomPlage")
+        self.horizontalLayout_2.addWidget(self.nomPlage)
         self.pollutionPlage = QtWidgets.QLabel(self.centralwidget)
-        self.pollutionPlage.setGeometry(QtCore.QRect(0, 160, 131, 31))
         self.pollutionPlage.setFrameShape(QtWidgets.QFrame.Box)
         self.pollutionPlage.setObjectName("pollutionPlage")
+        self.horizontalLayout_2.addWidget(self.pollutionPlage)
+        self.gridLayout_2.addLayout(self.horizontalLayout_2, 1, 0, 1, 3)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.plage = QtWidgets.QLabel(self.centralwidget)
-        self.plage.setGeometry(QtCore.QRect(30, 220, 601, 351))
         self.plage.setText("")
         self.plage.setPixmap(QtGui.QPixmap(":/resource/img/plage.JPG"))
+        self.plage.setAlignment(QtCore.Qt.AlignCenter)
         self.plage.setObjectName("plage")
+        self.horizontalLayout_3.addWidget(self.plage)
         self.spectre = QtWidgets.QLabel(self.centralwidget)
-        self.spectre.setGeometry(QtCore.QRect(650, 300, 421, 201))
+        self.spectre.setEnabled(True)
         self.spectre.setText("")
         self.spectre.setPixmap(QtGui.QPixmap(":/resource/img/spectre.png"))
+        self.spectre.setAlignment(QtCore.Qt.AlignCenter)
         self.spectre.setObjectName("spectre")
-        self.descriptionPlastique = QtWidgets.QLabel(self.centralwidget)
-        self.descriptionPlastique.setGeometry(QtCore.QRect(30, 600, 1061, 241))
-        self.descriptionPlastique.setFrameShape(QtWidgets.QFrame.Box)
-        self.descriptionPlastique.setObjectName("descriptionPlastique")
+        self.horizontalLayout_3.addWidget(self.spectre)
+        self.gridLayout_2.addLayout(self.horizontalLayout_3, 5, 0, 1, 3)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1115, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1126, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -71,12 +85,14 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.descriptionPlastique.setText(_translate("MainWindow", "Description du plastique"))
         self.Titre.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#00007f;\">Analyse des plages</span></p></body></html>"))
         self.nomPlage.setText(_translate("MainWindow", "Nom de la plage:"))
         self.pollutionPlage.setText(_translate("MainWindow", "Pollution de la plage:"))
-        self.descriptionPlastique.setText(_translate("MainWindow", "Description du plastique"))
+
 
 import resource_rc
+
 
 if __name__ == "__main__":
     import sys
@@ -86,4 +102,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
