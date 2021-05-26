7c3c383a17b2d4d2fece5d2978012fa5e0703556
alanglais <alix.delanglais@edu.esiee.fr>
2021-05-26 16:38:17 +0200

    new test scan

--- python/test_scan.py
+++ python/test_scan.py
@@ -1,9 +1,9 @@
 from Plage import Plage       #import de la classe Plage
 from Analyse import *
 
-my_plage = Plage(4,5,0.3)     #les parametres correspondent à : longeur(x), largeur(y), probabilité d'avoir du plastique.
+my_plage = Plage(4,3,0.3)     #les parametres correspondent à : longeur(x), largeur(y), probabilité d'avoir du plastique.
 my_plage.print_plage()
 
 my_analyse = Analyse()
-my_analyse.scan_plage(my_plage,liste_matrix_ref)
-
+#my_analyse.scan_plage(my_plage,liste_matrix_ref)
+my_analyse.affichage_scan(my_plage,liste_matrix_ref)
