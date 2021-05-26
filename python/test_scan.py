5357831f24157aaaf98f5341b92ce9d12399be77
alanglais <alix.delanglais@edu.esiee.fr>
2021-05-26 14:20:04 +0200

    ajout objet Analyse fichier test

--- python/test_scan.py
+++ python/test_scan.py
@@ -3,3 +3,6 @@ from Analyse import *
 
 my_plage = Plage(2,2,0.3)     #les parametres correspondent à : longeur(x), largeur(y), probabilité d'avoir du plastique.
 my_plage.print_plage()
+
+my_analyse = Analyse()
+my_analyse.scan_plage(my_plage,liste_matrix_ref)
