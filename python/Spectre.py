4af07057689862f91c92736335cd0b6f365ff95b
alanglais <alix.delanglais@edu.esiee.fr>
2021-05-26 16:25:34 +0200

    new test scan avec affichage proportions

--- python/Spectre.py
+++ python/Spectre.py
@@ -68,7 +68,10 @@ class Spectre:
                 return
             #print("Creation spectre plastique")
         else:
+<<<<<<< HEAD
             #print(len(liste_data_sable))
+=======
+>>>>>>> bfdedb7cca8cde5f5ec57860456e11c8c9913315
             liste_data = liste_data_sable
             #print("Creation spectre sable")
 
